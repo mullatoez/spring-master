a62ef6d7e4b41ee4b3d1b4283dd448e96346e7e5
mullatoez <kimanthikevin76@gmail.com>
2022-03-25 14:48:28 +0300

    consume rest api

--- src/main/java/com/daily_code_coolapp/controller/RestClient.java
+++ src/main/java/com/daily_code_coolapp/controller/RestClient.java
@@ -4,22 +4,38 @@ import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class RestClient {
-    public static final String GET_ALL_DEPARTMENTS_API = "http://localhost:8000/departments";
+    public static final String departments_url = "http://localhost:8000/departments";
 
     static RestTemplate restTemplate = new RestTemplate();
 
     public static void main(String[] args) {
-        callGetAllDepartmentsApi();
+        //getAllDepartments();
+       /* getDeprtmentsNew();*/
+
+        List<Object> departs = getDepartments();
+        System.out.println(departs);
     }
 
-    private static  void callGetAllDepartmentsApi(){
+    private static  void getAllDepartments(){
         HttpHeaders httpHeaders = new org.springframework.http.HttpHeaders();
         httpHeaders.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 
         HttpEntity<String> entity = new HttpEntity<>("parameters", httpHeaders);
-        ResponseEntity<String> responseEntity = restTemplate.exchange(GET_ALL_DEPARTMENTS_API, HttpMethod.GET,entity,String.class);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(departments_url, HttpMethod.GET,entity,String.class);
         System.out.print(responseEntity);
     }
+
+    static void getDeprtmentsNew(){
+        String res = restTemplate.getForObject(departments_url,String.class);
+        System.out.println(res);
+    }
+
+    static List<Object> getDepartments(){
+        Object[] deps = restTemplate.getForObject(departments_url,Object[].class);
+        return Arrays.asList(deps);
+    }
+
 }
